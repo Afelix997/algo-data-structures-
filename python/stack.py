fc804fc657263d3d9d51a093f0294f3fc04c7c2d
afelix997 <afelix2013@gmail.com>
2022-06-07 23:50:34 -0700

    working2

--- python/stack.py
+++ python/stack.py
@@ -1,14 +1,18 @@
 class Stack:
   # write your __init__ method here that should store a 'total' value which is the total number of elements in the Stack and a 'stack' value which is an array of stored values in the Stack
-
+  def __init__(self):
+    self.stack=[]
+    self.total= len(self.stack)
   def push(self, data):
     # write your code to add data following LIFO and return the Stack
-    pass
+    self.stack.append(data)
+    return self.stack
 
   def pop(self):
     # write your code to removes the data following LIFO and return the Stack
-    pass
+    self.stack.pop(-1)
+    return self.stack
 
   def size(self):
     # write your code that returns the size of the Stack
-    pass
+    return self.total
